4761a1326cff6cfd2cfdd80cdd955237eb31741a
Martin laptop <Martin.Andre.Jarneid@ssn.no>
2023-02-09 15:40:33 +0100

    Add working game

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import { useState } from "react";
 import "./App.css";
 import GameBoard from "./components/GameBoard";
+import ScoreBoard from "./components/ScoreBoard";
 
 function App() {
+    const [score, setScore] = useState(0);
+    const [hiScore, setHiScore] = useState(0);
+    const [gameOver, setGameOver] = useState(false);
+
+    const incrementScore = () => {
+        if (gameOver) setGameOver(false);
+        setScore(score + 1);
+    };
+
+    const restartGame = () => {
+        if (score > hiScore) {
+            setHiScore(score);
+        }
+        setScore(0);
+        setGameOver(true);
+    };
+
     return (
         <>
-            <GameBoard amount={12} />
+            <ScoreBoard score={score} hiScore={hiScore} />
+            <GameBoard
+                amount={12}
+                incrementScore={incrementScore}
+                gameOver={gameOver}
+                restart={restartGame}
+            />
         </>
     );
 }
