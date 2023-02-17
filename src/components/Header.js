f44e6c907d81ba91ef57a058e5585ef8542ac660
Martin laptop <Martin.Andre.Jarneid@ssn.no>
2023-02-17 13:32:16 +0100

    Hide header when scrolling down

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import AmountPicker from "./AmountPicker";
 import ScoreBoard from "./ScoreBoard";
 import Switcher from "./Switcher";
@@ -10,6 +11,43 @@ export default function Header({
     switchAction,
     switchState,
 }) {
+    // Hide header when scrolling down
+    useEffect(() => {
+        const header = document.querySelector("header");
+        const headerHeight = header.offsetHeight;
+        let lastScrollPos = window.scrollY;
+        let currentScrollPos, scrollDir, lastScrollDir;
+
+        const scrollFunc = () => {
+            currentScrollPos = window.scrollY;
+            if (currentScrollPos > lastScrollPos) {
+                scrollDir = "down";
+            } else if (currentScrollPos < lastScrollPos) {
+                scrollDir = "up";
+            }
+
+            toggleHeader(scrollDir, currentScrollPos);
+            lastScrollDir = scrollDir;
+            lastScrollPos = currentScrollPos;
+        };
+
+        const toggleHeader = (direction, scrollPos) => {
+            if (direction === "down" && scrollPos > headerHeight) {
+                if (!header.classList.contains("hide")) {
+                    header.classList.add("hide");
+                }
+            } else if (direction === "up") {
+                if (header.classList.contains("hide"))
+                    header.classList.remove("hide");
+            }
+        };
+
+        window.addEventListener("scroll", scrollFunc);
+
+        return () => {
+            window.removeEventListener("scroll", scrollFunc);
+        };
+    }, []);
     return (
         <header>
             <AmountPicker onChange={changeAmount} amount={amount} />
