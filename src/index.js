b5fa101554cba60ceaa33599f238c91d7f47edad
Martin laptop <Martin.Andre.Jarneid@ssn.no>
2023-02-22 14:05:49 +0100

    Make PWA

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from "react-dom/client";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
+serviceWorkerRegistration.register();
